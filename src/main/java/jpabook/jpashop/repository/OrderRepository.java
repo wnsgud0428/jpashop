b3460d7e0cb219a4e0ffaab0e5ab1dec4424b741
wnsgud0428 <wnsgud0428@naver.com>
2024-01-11 22:29:27 +0900

    (cont) findAllByString 완성

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -4,8 +4,11 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.lang.reflect.Type;
 import java.util.List;
 
 @Repository
@@ -22,11 +25,48 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // todo: 검색기능
     public List<Order> findAllByString(OrderSearch orderSearch) {
+        // language=JPQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+            return query.getResultList();
 
     }
-    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
 
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        // 실무에서는 복잡해서 사용하지 않음 -> Querydsl 사용!
     }
 }
