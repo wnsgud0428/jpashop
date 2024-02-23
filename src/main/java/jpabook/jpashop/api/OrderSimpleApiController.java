27ca7771989f04cbec0cf55ef630ae2a77da2496
wnsgud0428 <wnsgud0428@naver.com>
2024-02-23 22:14:26 +0900

    간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +28,7 @@ public class OrderSimpleApiController {
 
     //    private final OrderService orderService;
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     /**
      * V1. 엔티티 직접 노출
@@ -57,7 +60,6 @@ public class OrderSimpleApiController {
         return result;
     }
 
-
     /**
      * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
      * - fetch join으로 쿼리 1번 호출
@@ -73,17 +75,27 @@ public class OrderSimpleApiController {
         return result;
     }
 
+    /**
+     * V4. JPA에서 DTO로 바로 조회
+     * - 쿼리 1번 호출
+     * - select 절에서 원하는 데이터만 선택해서 조회
+     */
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
     @Data
     static class SimpleOrderDto {
         private Long orderId;
-        private String ordererName;
+        private String name;
         private LocalDateTime orderDate;
         private OrderStatus orderStatus;
         private Address address;
 
         public SimpleOrderDto(Order order) {
             orderId = order.getId();
-            ordererName = order.getMember().getName();
+            name = order.getMember().getName();
             orderDate = order.getOrderDate();
             orderStatus = order.getStatus();
             address = order.getDelivery().getAddress();
