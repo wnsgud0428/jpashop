60bfbea6369fef2e2b733f61af3fbbcff238ab74
wnsgud0428 <wnsgud0428@naver.com>
2024-02-23 21:30:15 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,14 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
-import jpabook.jpashop.service.OrderService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne) 관계 최적화
@@ -20,7 +24,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
 
-//    private final OrderService orderService;
+    //    private final OrderService orderService;
     private final OrderRepository orderRepository;
 
     /**
@@ -29,7 +33,7 @@ public class OrderSimpleApiController {
      * - 양방향 관계 문제 발생 -> @JsonIgnore
      */
     @GetMapping("/api/v1/simple-orders")
-    public List<Order> ordersV1 () {
+    public List<Order> ordersV1() {
 //        List<Order> orders = orderService.findOrders(new OrderSearch());
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
@@ -38,4 +42,36 @@ public class OrderSimpleApiController {
         }
         return all;
     }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출 (N+1 문제)
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String ordererName;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            ordererName = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
 }
