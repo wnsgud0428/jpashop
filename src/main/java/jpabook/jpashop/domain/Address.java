c6c4e9f4e8e7c59878ea4f391cb18806c3bb028a
wnsgud0428 <wnsgud0428@naver.com>
2024-03-03 20:23:07 +0900

    .

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -16,6 +16,8 @@ public class Address {
 
     }
 
+    // 값 타입은 변경 불가능하게 설계해야 함
+    // @Setter를 제거하고, 생성자를 통해서 값을 초기화하도록 한다 -> 변경 불가능한 클래스
     public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
